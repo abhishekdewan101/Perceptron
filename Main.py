508a99f04ca4fd76b2d601cb065d4d85110f4ee0
Abhishek Dewan <adewan@umich.edu>
2019-07-28 22:15:24 -0700

    Added a graph to show the difference

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from Perceptron import Perceptron
 from Point import Point
+import matplotlib.pyplot as plt
+import numpy as np
 
 brain = Perceptron(2)
 inputs = [-1, 0.5]
@@ -23,10 +25,17 @@ numberCorrect = 0
 for x in range(0, 500):
     points[x] = Point(width, height)
 
+data = np.zeros(shape=[500, 3], dtype=object)
+
 # ask the brain to guess the correct output and if it fails then don't count towards accuracy
 for x in range(0, 500):
     guess = brain.guess([points[x].x, points[x].y])
     if guess == points[x].label:
         numberCorrect = numberCorrect + 1
+        data[x] = [points[x].x, points[x].y, 'green']
+    else:
+        data[x] = [points[x].x, points[x].y, 'red']
 
+plt.scatter(data[:, 0], data[:, 1], marker='o', c=data[:,2])
+plt.show()
 print('Perceptron Accuracy is: ' + str((numberCorrect / 500) * 100))
